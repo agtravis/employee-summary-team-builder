6aa21b3f9450a184691baa0b61427d46ebe043d1
George Travis <wewerelikesharks@gmail.com>
2020-02-01 19:19:21 -0800

    added engineer prompts

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
-
 const functions = require('./lib/functions');
 const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
 const inquirer = require('inquirer');
+
+const finalTeam = [];
 
 class Team {
   constructor() {}
   async start() {
+    console.log('Team building commencing...');
     const managerInfo = await functions.addManager();
     this.manager = new manager.Manager(
       managerInfo.managerName,
@@ -15,6 +18,7 @@ class Team {
       managerInfo.managerEmail,
       managerInfo.officeNumber
     );
+    finalTeam.push(this.manager);
     this.addEmployee();
   }
   async addEmployee() {
@@ -30,14 +34,24 @@ class Team {
         this.completeTeam();
     }
   }
-  addEngineer() {
-    console.log(`Engineer added!`);
+  async addEngineer() {
+    const engineerInfo = await functions.addEngineer();
+    this.engineer = new engineer.Engineer(
+      engineerInfo.engineerName,
+      engineerInfo.engineerId,
+      'Engineer',
+      engineerInfo.engineerEmail,
+      engineerInfo.gitHub
+    );
+    finalTeam.push(this.engineer);
+    this.addEmployee();
   }
   addIntern() {
     console.log(`Intern added!`);
   }
   completeTeam() {
     console.log(`Team Complete!`);
+    console.log(finalTeam);
   }
 }
 
